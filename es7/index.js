9c3ee6ef97304ff4d03d2ea6b1f1772ff2c58a55
Edward Brito <Edwardbrito11@hotmail.com>
2021-06-12 21:51:39 -0400

    Lo nuevo en ECMAScript 7

---
+++ es7/index.js
@@ -0,0 +1,16 @@
+//includes
+let numbers = [1, 2, 3, 7, 8];
+
+if (numbers.includes(9)) {
+    console.log('Si se encuentra el valor 7');
+} else {
+    console.log('No se encuentra.')
+}
+
+//pow
+let base = 4;
+let exponent = 4;
+let result = base ** exponent;
+
+
+console.log(result);
