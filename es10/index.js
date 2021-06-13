6f1c0dce5d96845537ca49c48cf2acdc495915a2
Edward Brito <Edwardbrito11@hotmail.com>
2021-06-12 21:52:09 -0400

    Lo nuevo en ECMAScript 10

---
+++ es10/index.js
@@ -0,0 +1,33 @@
+let array = [1, 2, 3, [1, 2, 3, [1, 2, 3]]];
+
+console.log(array.flat(2));
+
+let array1 = [1, 2, 3, 4, 5];
+
+console.log(array1.flatMap(value => [value, value * 2]));
+
+let hello = '        hello world';
+console.log(hello);
+console.log(hello.trimStart());
+
+let hello1 = 'hello World       ';
+console.log(hello1);
+console.log(hello.trimEnd());
+
+try {
+
+} catch {
+    error
+}
+
+
+let entries = [["name", "oscar"], ["age", 32]];
+console.log(Object.fromEntries(entries));
+
+let mySymbl = `My Symbol`;
+let symbol = Symbol(mySymbl);
+console.log(symbol.description);
+
+let b = 'a'
+let b = 'b'
+console.log(b)
